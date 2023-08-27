b99daa179301807a49acba56bf72df720ad2e7a8
sonoff2 <v.flament.59@gmail.com>
2023-08-27 23:55:32 +0200

    initial commit

---
+++ utils.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+import logging
+import re
+import rapidfuzz
+
+def setup_logging():
+    # 'INFO' level by default, could be specified by the user (for later)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="goodreads_rebot")
+    parser.add_argument('--config', type=str, required=True, help="Path to configuration JSON file")
+    args = parser.parse_args()
+    return args
+
+def load_config(args):
+    """
+    Load config dictionary from json path provided by user
+    :param args: arguments parsed (with argparse library)
+    :return: Dictionary
+    """
+    try:
+        with open(args.config, 'r') as json_file:
+            config_dict = json.load(json_file)
+        return config_dict
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(f"The file {args.config} is not a valid JSON file: {str(e)}")
+
+def extract_braces(comment_body):
+    sub_parts = re.findall(r'\{\{([^}]*)\}\}', comment_body)
+    return sub_parts
+
+def partial_ratio(s1, s2):
+    return rapidfuzz.fuzz.partial_ratio(str.upper(s1), str.upper(s2))
