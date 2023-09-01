d8a25ac7b8bc1c11038f5f0d339c470d67df315f
sonoff2 <v.flament.59@gmail.com>
2023-09-02 00:08:05 +0200

    First full version to debug

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import bot
-import firestore
-import praw_wrapper
-import time
 import utils
 import logging
+from utils import config
 
 
 def main():
@@ -11,30 +9,18 @@ def main():
     # Start logging
     utils.setup_logging()
 
-    # Parse arguments and load config
-    logging.info("Parsing arguments and loading config")
-    args = utils.parse_arguments()
-    config = utils.load_config(args)
-
     # Intiate instances
-    reddit = praw_wrapper.init(config)
-    db, app = firestore.init()
-    grbot = bot.Bot(reddit, db)
+    grbot = bot.Bot(config)
 
     # 1) Crawl comments
-    config["last_timestamp"] = firestore.get_last_timestamp()
-    comments = grbot.crawl_and_parse(config)
+    if config["flow"]["run_reader"]:
+        logging.info("Started Crawling")
+        grbot.run_crawling()
 
-    # Calculate the from_timestamp based on your requirements
-    from_timestamp = "XXX"
-
-    while True:
-        grbot.run('Bonsai', from_timestamp)  # Replace 'Bonsai' with your desired subreddit name
-
-        # Update the last_timestamp in Firestore
-        firestore.update_last_timestamp(db, from_timestamp)
-
-
+    # 2) Answer one
+    if config["flow"]["run_poster"]:
+        logging.info("Started Matching")
+        grbot.match_and_reply_one()
 
 if __name__ == "__main__":
     main()
