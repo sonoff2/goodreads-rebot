0f8ef2e966995ca5be79476eb5ca51f7b811f21c
sonoff2 <v.flament.59@gmail.com>
2023-09-02 00:13:44 +0200

    First full version to debug

--- praw_wrapper.py
+++ praw_wrapper.py
@@ -2,8 +2,10 @@ import praw
 
 def init(config):
     reddit = praw.Reddit(
-      client_id=config['creds']['reddit_client_id'],
-      client_secret=config['creds']['reddit_client_secret'],
-       #...
+        client_id=config['creds']['reddit_client_id'],
+        client_secret=config['creds']['reddit_client_secret'],
+        user_agent=config['creds']['reddit_user_agent'],
+        username=config['creds']['reddit_username'],
+        password=config['creds']['reddit_password'],
     )
     return reddit
