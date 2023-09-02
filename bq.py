87eee8d580ab9a3aff25f79db8e0cdcc0990aa2f
sonoff2 <v.flament.59@gmail.com>
2023-09-03 00:17:29 +0200

    working in local

--- bq.py
+++ bq.py
@@ -1,18 +1,27 @@
 from google.cloud import bigquery
+from google.oauth2 import service_account
+from google.cloud.bigquery.schema import SchemaField
 from utils import config
 import pandas as pd
+import logging
 
 TABLE_DIM = config['bq']['table_dim']
 TABLE_TO_MATCH = config['bq']['table_to_match']
 TABLE_CRAWL_DATES = config['bq']['table_crawl_dates']
 
-client = bigquery.Client()
+if config['flow']['mode'] == 'local':
+    credentials = service_account.Credentials.from_service_account_file(
+        config['creds']['bq_path'], scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+else:
+    client = bigquery.Client()
 
-def append_to_table(df, table, string_columns=[], client=client):
+def append_to_table(df, table, schema_dic, client=client):
     table_obj = client.dataset(table.split('.')[0]).table(table.split('.')[1])
-    job_config = bigquery.LoadJobConfig(schema=[
-        bigquery.SchemaField(string_col, "STRING") for string_col in string_columns])
-    errors = client.insert_rows_from_dataframe(table_obj, df, job_config=job_config)
+    schema = [
+        SchemaField(name=column, field_type=typ) for (column,typ) in schema_dic.items()
+    ]
+    errors = client.insert_rows_from_dataframe(table_obj, df, selected_fields=schema)
     if errors == []:
         print("Rows appended successfully")
     else:
@@ -20,7 +29,8 @@ def append_to_table(df, table, string_columns=[], client=client):
     return errors
 
 def delete_from_table(column, my_list, table, client=client):
-    delete_query = f"""DELETE FROM {table} WHERE {column} IN ({"', '".join(my_list)})"""
+    delete_query = f"""DELETE FROM {table} WHERE CAST({column} AS STRING) IN ('{"', '".join(my_list)}')"""
+    logging.info(f"Running query : {delete_query}")
     query_job = client.query(delete_query)
     if query_job.errors == []:
         print("Rows deleted successfully")
@@ -29,6 +39,7 @@ def delete_from_table(column, my_list, table, client=client):
     return query_job.errors
 
 def sql_to_df(query, client=client):
+    logging.info(f"""Attempting to run the query : "{query.strip()}" """)
     return client.query(query).result().to_dataframe()
 
 def get_books_by_author(table=TABLE_DIM):
@@ -41,11 +52,9 @@ def get_books_by_author(table=TABLE_DIM):
 
 def get_all_titles(table=TABLE_DIM):
     query = f"""
-        SELECT lower(title) as title FROM {table} ORDER BY n_sugg DESC
+        SELECT lower(title_short) as title_short FROM {table} ORDER BY n_sugg DESC
     """
-    df = sql_to_df(query)
-    df['title'] = df['title'].apply(lambda x: x.split("(")[0].strip())
-    return df
+    return list(sql_to_df(query)['title_short'])
 
 def get_last_timestamp(subreddit, table=TABLE_CRAWL_DATES):
     query = f"""
@@ -58,14 +67,14 @@ def update_timestamp(subreddit, timestamp, table=TABLE_CRAWL_DATES):
     return append_to_table(
         df=pd.DataFrame(data={'subreddit': [subreddit], 'crawl_timestamp': [int(timestamp)]}),
         table=table,
-        string_columns=['subreddit']
+        schema_dic = {'subreddit': 'STRING', 'crawl_timestamp': 'INTEGER'}
     )
 
 def save_comment_ids_to_match(comment_ids, table=TABLE_TO_MATCH):
     return append_to_table(
         df=pd.DataFrame(comment_ids, columns = ["subreddit", "comment_id", "comment_timestamp"]),
         table=table,
-        string_columns=["subreddit", "comment_id"]
+        schema_dic={"subreddit": "STRING", "comment_id": 'STRING', 'comment_timestamp': 'INTEGER'}
     )
 
 def get_comment_ids_to_match(subreddit, table=TABLE_TO_MATCH):
@@ -74,8 +83,13 @@ def get_comment_ids_to_match(subreddit, table=TABLE_TO_MATCH):
     """)['comment_id'])
 
 def remove_comment_ids_to_match(ids, table=TABLE_TO_MATCH):
+    logging.info(f"Deleting ids {ids} from table {TABLE_TO_MATCH}")
     return delete_from_table(
         column='comment_id',
         my_list=ids,
         table=table
     )
+
+def get_book_info(title, table=TABLE_DIM, order_by='n_sugg'):
+    return sql_to_df(
+        f"""SELECT * FROM {table} WHERE LOWER(title_short) = '{title}' ORDER BY {order_by} DESC""").iloc[0].to_dict()
