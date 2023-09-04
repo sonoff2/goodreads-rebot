f1e1f8fd863a3392721da9ac39e9ed526971c4da
sonoff2 <v.flament.59@gmail.com>
2023-09-05 00:34:19 +0200

    updated tables queries and schemas to work on comments and submissions + work on book series

--- bq.py
+++ bq.py
@@ -4,6 +4,7 @@ from google.cloud.bigquery.schema import SchemaField
 from utils import config
 import pandas as pd
 import logging
+from typing import List, Any
 
 TABLE_DIM = config['bq']['table_dim']
 TABLE_TO_MATCH = config['bq']['table_to_match']
@@ -56,6 +57,12 @@ def get_all_titles(table=TABLE_DIM):
     """
     return list(sql_to_df(query)['title_short'])
 
+def get_series_titles(table=TABLE_DIM):
+    query = f"""
+        SELECT lower(title_series) as title_series from {table} WHERE title_series is not null
+    """
+    return list(sql_to_df(query)['title_series'])
+
 def get_last_timestamp(subreddit, table=TABLE_CRAWL_DATES):
     query = f"""
     SELECT MAX(crawl_timestamp) as timestamp FROM {table} WHERE subreddit = '{subreddit}'
@@ -70,26 +77,31 @@ def update_timestamp(subreddit, timestamp, table=TABLE_CRAWL_DATES):
         schema_dic = {'subreddit': 'STRING', 'crawl_timestamp': 'INTEGER'}
     )
 
-def save_comment_ids_to_match(comment_ids, table=TABLE_TO_MATCH):
+def save_post_ids_to_match(post_ids, table=TABLE_TO_MATCH):
     return append_to_table(
-        df=pd.DataFrame(comment_ids, columns = ["subreddit", "comment_id", "comment_timestamp"]),
+        df=pd.DataFrame(post_ids, columns = ["subreddit", "post_id", "post_timestamp", "post_type"]),
         table=table,
-        schema_dic={"subreddit": "STRING", "comment_id": 'STRING', 'comment_timestamp': 'INTEGER'}
+        schema_dic={"subreddit": "STRING", "post_id": 'STRING', 'post_timestamp': 'INTEGER', "post_type": 'STRING'}
     )
 
-def get_comment_ids_to_match(subreddit, table=TABLE_TO_MATCH):
-    return list(sql_to_df(f"""
-        SELECT * FROM {table} WHERE subreddit = '{subreddit}' ORDER BY comment_timestamp DESC
-    """)['comment_id'])
+def get_post_ids_to_match(subreddit, table=TABLE_TO_MATCH):
+    return sql_to_df(f"""
+        SELECT * FROM {table} WHERE subreddit = '{subreddit}' ORDER BY post_timestamp DESC
+    """)[['post_id', 'post_type']].values.tolist()
 
-def remove_comment_ids_to_match(ids, table=TABLE_TO_MATCH):
+def remove_post_ids_to_match(ids, table=TABLE_TO_MATCH):
     logging.info(f"Deleting ids {ids} from table {TABLE_TO_MATCH}")
     return delete_from_table(
-        column='comment_id',
+        column='post_id',
         my_list=ids,
         table=table
     )
 
-def get_book_info(title, table=TABLE_DIM, order_by='n_sugg'):
+def get_book_info(title, is_series, table=TABLE_DIM, order_by='n_sugg'):
+    if is_series:
+        col_sql = 'title_series'
+    else:
+        col_sql = 'title_short'
     return sql_to_df(
-        f"""SELECT * FROM {table} WHERE LOWER(title_short) = '{title}' ORDER BY {order_by} DESC""").iloc[0].to_dict()
+            f"""SELECT * FROM {table} WHERE LOWER({col_sql}) = '{title}' ORDER BY {order_by} DESC"""
+        ).iloc[0].to_dict()
