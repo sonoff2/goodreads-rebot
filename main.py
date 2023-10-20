f2abf1517315a6ab809b64076139f8a006f33810
sonoff2 <v.flament.59@gmail.com>
2023-10-20 11:09:56 +0200

    grbot is now a package

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import bot
-import utils
+from grbot import bot, utils
 import logging
-from utils import config
+from grbot.utils import config
 
 
 def main():
@@ -10,17 +9,17 @@ def main():
     utils.setup_logging()
 
     # Intiate instances
-    grbot = bot.Bot(config)
+    my_bot = bot.Bot(config)
 
     # 1) Crawl comments
     if config["flow"]["run_reader"]:
         logging.info("Started Crawling")
-        grbot.run_crawling()
+        my_bot.run_crawling()
 
     # 2) Answer one
     if config["flow"]["run_poster"]:
         logging.info("Started Matching")
-        grbot.match_and_reply_one()
+        my_bot.match_and_reply_one()
 
 if __name__ == "__main__":
     main()
